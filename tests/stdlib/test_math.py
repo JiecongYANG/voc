0f1ddd9f81f7329d30ab8edb58c5a9e47ec08551
Carl Enlund <carl.enlund@gmail.com>
2021-09-08 13:11:48 +0200

    Fix import statement for math tests

--- tests/stdlib/test_math.py
+++ tests/stdlib/test_math.py
@@ -1,31 +1,22 @@
-from unittest import expectedFailure
+import unittest
 
-from .. utils import TranspileTestCase
+from ..utils import TranspileTestCase
 
 class MathModuleTests(TranspileTestCase):
     def test_trunc1(self):
         self.assertCodeExecution("""
-            import math
+            from math import trunc
             x = 3.1
             y = 0.9
-            z = -1.1
-            w = -0.9999999999999999999999999999999999
-            print(math.trunc(x))
-            print(math.trunc(y))
-            print(math.trunc(z))
-            print(math.trunc(w))
+            print(trunc(x))
+            print(trunc(y))
         """)
 
-    @expectedFailure
     def test_trunc2(self):
         self.assertCodeExecution("""
-            import math
-            print(math.trunc('not a number'))
-        """)
-
-    @expectedFailure
-    def test_trunc3(self):
-        self.assertCodeExecution("""
-            import math
-            print(math.trunc(['not', 'a', 'number']))
+            from math import trunc
+            x = -1.1
+            y = -0.9999999999999999999999999999999999
+            print(trunc(x))
+            print(trunc(y))
         """)

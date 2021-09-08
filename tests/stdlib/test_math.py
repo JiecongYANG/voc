0ccdf7268d3b669f10bc66f37993e4d470b6913d
Carl Enlund <carl.enlund@gmail.com>
2021-09-08 11:49:05 +0200

    Update test_math.py

--- tests/stdlib/test_math.py
+++ tests/stdlib/test_math.py
@@ -1,31 +1,31 @@
 from unittest import expectedFailure
 
-from .. utils import TranspileTestCase, BuiltinFunctionTestCase, BuiltinTwoargFunctionTestCase
+from .. utils import TranspileTestCase
 
-class MathTests(TranspileTestCase):
+class MathModuleTests(TranspileTestCase):
     def test_trunc1(self):
         self.assertCodeExecution("""
-            from math import trunc
+            import math
             x = 3.1
             y = 0.9
             z = -1.1
             w = -0.9999999999999999999999999999999999
-            print(trunc(x))
-            print(trunc(y))
-            print(trunc(z))
-            print(trunc(w))
+            print(math.trunc(x))
+            print(math.trunc(y))
+            print(math.trunc(z))
+            print(math.trunc(w))
         """)
 
-    # @expectedFailure
-    # def test_trunc2(self):
-    #     self.assertCodeExecution("""
-    #         import math
-    #         print(math.trunc('not a number'))
-    #     """)
+    @expectedFailure
+    def test_trunc2(self):
+        self.assertCodeExecution("""
+            import math
+            print(math.trunc('not a number'))
+        """)
 
-    # @expectedFailure
-    # def test_trunc3(self):
-    #     self.assertCodeExecution("""
-    #         import math
-    #         print(math.trunc(['not', 'a', 'number']))
-    #     """)
+    @expectedFailure
+    def test_trunc3(self):
+        self.assertCodeExecution("""
+            import math
+            print(math.trunc(['not', 'a', 'number']))
+        """)

7397ea97e4d62ae697dde204ff12efa537b91f34
Carl Enlund <carl.enlund@gmail.com>
2021-09-08 20:13:27 +0200

    Should work now, but still gives NameError

--- tests/stdlib/test_math.py
+++ tests/stdlib/test_math.py
@@ -5,18 +5,18 @@ from ..utils import TranspileTestCase
 class MathModuleTests(TranspileTestCase):
     def test_trunc1(self):
         self.assertCodeExecution("""
-            from math import trunc
+            import math
             x = 3.1
             y = 0.9
-            print(trunc(x))
-            print(trunc(y))
+            print(math.trunc(x))
+            print(math.trunc(y))
         """)
 
     def test_trunc2(self):
         self.assertCodeExecution("""
-            from math import trunc
+            import math
             x = -1.1
             y = -0.9999999999999999999999999999999999
-            print(trunc(x))
-            print(trunc(y))
+            print(math.trunc(x))
+            print(math.trunc(y))
         """)

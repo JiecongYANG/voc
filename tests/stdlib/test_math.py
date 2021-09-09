62e339c833f3a889e6a30dc8d78ef5d9148513a5
Carl Enlund <carl.enlund@gmail.com>
2021-09-09 12:37:45 +0200

    Update test_math.py

--- tests/stdlib/test_math.py
+++ tests/stdlib/test_math.py
@@ -3,20 +3,31 @@ import unittest
 from ..utils import TranspileTestCase
 
 class MathModuleTests(TranspileTestCase):
-    def test_trunc1(self):
+    def test_trunc_positive(self):
         self.assertCodeExecution("""
             import math
             x = 3.1
             y = 0.9
+            z = 2
             print(math.trunc(x))
             print(math.trunc(y))
+            print(math.trunc(z))
         """)
 
-    def test_trunc2(self):
+    def test_trunc_negative(self):
         self.assertCodeExecution("""
             import math
             x = -1.1
             y = -0.9999999999999999999999999999999999
+            z = -2
             print(math.trunc(x))
             print(math.trunc(y))
+            print(math.trunc(z))
+        """)
+
+    @unittest.expectedFailure
+    def test_trunc_exception(self):
+        self.assertCodeExecution("""
+            import math
+            print(math.trunc('not a number'))
         """)

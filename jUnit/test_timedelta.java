b1ea9c98cdca1859ae67a8dacafa4eec07113e22
Carl Enlund <carl.enlund@gmail.com>
2021-09-17 13:59:21 +0200

    Add jUnit tests

---
+++ jUnit/test_timedelta.java
@@ -0,0 +1,34 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class test_timedelta {
+    @Test
+    public void testEqual() {
+        assert(false);
+    }
+
+    @Test
+    public void testNotEqual() {
+        assert(false);
+    }
+
+    @Test
+    public void testLessThan() {
+        assert(false);
+    }
+
+    @Test
+    public void testGreaterThan() {
+        assert(false);
+    }
+
+    @Test
+    public void testLessThanOrEqual() {
+        assert(false);
+    }
+
+    @Test
+    public void testGreaterThanOrEqual() {
+        assert(false);
+    }
+}
